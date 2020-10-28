4736d947b313a66f68f87a7d522714ff83eb9efa
16itmohit.manglani@gmail.com <mohitmanglani2906@gmail.com>
2020-10-28 23:13:50 +0530

    changes pushed

---
+++ src/app/menubar/menubar.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+declare var openNav: any;
+
+@Component({
+  selector: 'app-menubar',
+  templateUrl: './menubar.component.html',
+  styleUrls: ['./menubar.component.css']
+})
+export class MenubarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+    //new openNav();
+  }
+
+}
