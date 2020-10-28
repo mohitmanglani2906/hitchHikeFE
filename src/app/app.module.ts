70617ce5a04897b51a14cba6dd5ae551eac31b0c
16itmohit.manglani@gmail.com <mohitmanglani2906@gmail.com>
2020-10-28 23:12:39 +0530

    components created for dashboard and menubar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { MenubarComponent } from './menubar/menubar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    HeaderComponent,
+    FooterComponent,
+    MenubarComponent
   ],
   imports: [
     BrowserModule,
